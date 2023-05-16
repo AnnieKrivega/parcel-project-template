6b125a54070fd145152c7a1ae47b9381521fe022
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-05-16 13:49:55 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist.simple-lightbox.min.css'
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+const items = [];
+
+galleryItems.forEach(element => {
+    const galleryItem = document.createElement('div')
+    galleryItem.className = 'gallery__item'
+    const galleryLink = document.createElement('a')
+    galleryLink.className = 'gallery__link'
+    galleryLink.href = element.original
+    const galleryImage = document.createElement('img')
+    galleryImage.className = 'gallery__image'
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute('data-source', element.original)
+    galleryImage.set = element.description;
+
+    galleryItem.append(galleryLink)
+    galleryLink.append(galleryImage)
+    items.push(galleryItem)
+})
+
+gallery.append(...items)
+
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+})
--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+const UPDATE_FREQUENCY = 1000; 
+
+
+const saveCurrentTime = throttle(time => {
+localStorage.setItem(STORAGE_KEY, time);
+}, UPDATE_FREQUENCY);
+
+player.on('timeupdate', event => {
+const currentTime = event.seconds;
+saveCurrentTime(currentTime);
+});
+
+const restoreCurrentTime = () => {
+const currentTime = localStorage.getItem(STORAGE_KEY);
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
+};
+
+restoreCurrentTime();
